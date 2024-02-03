d75dcae0c8a4e46d762619c4bd9e9e5c6f58da4d
taha <psiko769@gmail.com>
2024-02-03 12:03:02 +0100

    modifier le paddin de projectcard

--- src/Components/Projects/ProjectsCardA/ProjectsCardA.jsx
+++ src/Components/Projects/ProjectsCardA/ProjectsCardA.jsx
@@ -6,9 +6,10 @@ const ProjectsCardA = ({details}) => {
   return (
     <div className='project-card'>
         <h6>{details.title}</h6>
-        <img src={details.img} alt="" />
+        <img  src={details.img} alt="" />
         <h4 >Description : </h4>
-        <div className='description'>{check?details.description?.slice(0,60):details.description}<button className='see-more' onClick={()=>{setChek(prev=>!prev)}} >{check?"...read more":" see less"}</button></div>
+        <div className='description' title={check? details.description?.slice(60,-1):""}>  
+        {check?details.description?.slice(0,60) :details.description}<button className='see-more' onClick={()=>{setChek(prev=>!prev)}} >{check?"...read more":" see less"}</button></div>
         <button className='visit'><a  target="_blank" href={details.link}> Visit the project </a></button>
         
     </div>
